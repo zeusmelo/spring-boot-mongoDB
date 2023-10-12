177533fb7f788d84db881d22e7e8baac9a7ebf94
Zeus Policarpo <zeusbr26@hotmail.com>
2023-10-11 23:33:46 -0300

    refer to posts

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -42,9 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Viajarei para são paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("11/04/2018"), "vacation time", "i'm so glad, god", new AuthorDTO(maria));
-
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		
+		userRepository.save(maria);
+		
 	}
 
 }
