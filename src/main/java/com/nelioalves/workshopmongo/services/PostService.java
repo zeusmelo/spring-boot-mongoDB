1c0f125d567ccf62c8d6f44911b6581bb547271e
Zeus Policarpo <zeusbr26@hotmail.com>
2023-10-12 18:32:45 -0300

    finished

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.services;
 
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,4 +30,9 @@ public class PostService {
 	
 	
 
+	@SuppressWarnings("deprecation")
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getDate() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
