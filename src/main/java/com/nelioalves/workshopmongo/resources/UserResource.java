96ea3abd7ca29e6a9de52efe575b5b56a16fa4c3
Zeus Policarpo <zeusbr26@hotmail.com>
2023-10-12 00:03:31 -0300

    endpoint for users posts

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
@@ -68,5 +69,14 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 
 	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.fidByid(id);
+
+		
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+
 
 }
